d02e708ce79b5faa3093e7187690137178329143
Gerardo Saca <gsacavdm@microsoft.com>
2017-05-03 11:35:17 -0700

    Fix for Azure websites

--- index.js
+++ index.js
@@ -59,6 +59,7 @@ app.get("/hello",
     }
 );
 
-app.listen(5000, function () {
-    console.log("Listening on port 5000");
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+    console.log("Listening on port " + port);
 });
