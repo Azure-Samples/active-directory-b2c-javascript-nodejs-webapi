799b48cf1c66435c30269de5fadcf3a4579fe3ba
Michal Polkowski <michalp@speechmatics.com>
2022-02-01 23:13:33 +0000

    changed underscores to camels

--- sampleUsageData.js
+++ sampleUsageData.js
@@ -5,50 +5,50 @@ const sampleJson = {
     {
       month: 7,
       year: 2021,
-      usage_normal: 0.5,
-      limit_normal: 3,
-      usage_enhanced: 1.5,
-      limit_enhanced: 3
+      usageNormal: 0.5,
+      limitNormal: 3,
+      usageEnhanced: 1.5,
+      limitEnhanced: 3
     },
     {
       month: 8,
       year: 2021,
-      usage_normal: 1.5,
-      limit_normal: 3,
-      usage_enhanced: 0.5,
-      limit_enhanced: 3
+      usageNormal: 1.5,
+      limitNormal: 3,
+      usageEnhanced: 0.5,
+      limitEnhanced: 3
     },
     {
       month: 9,
       year: 2021,
-      usage_normal: 2.5,
-      limit_normal: 3,
-      usage_enhanced: 1.5,
-      limit_enhanced: 3
+      usageNormal: 2.5,
+      limitNormal: 3,
+      usageEnhanced: 1.5,
+      limitEnhanced: 3
     },
     {
       month: 10,
       year: 2021,
-      usage_normal: 1.5,
-      limit_normal: 3,
-      usage_enhanced: 2.5,
-      limit_enhanced: 3
+      usageNormal: 1.5,
+      limitNormal: 3,
+      usageEnhanced: 2.5,
+      limitEnhanced: 3
     },
     {
       month: 11,
       year: 2021,
-      usage_normal: 2.9,
-      limit_normal: 3,
-      usage_enhanced: 1.9,
-      limit_enhanced: 3
+      usageNormal: 2.9,
+      limitNormal: 3,
+      usageEnhanced: 1.9,
+      limitEnhanced: 3
     },
     {
       month: 12,
       year: 2021,
-      usage_normal: 2.5,
-      limit_normal: 3,
-      usage_enhanced: 0.3,
-      limit_enhanced: 3
+      usageNormal: 2.5,
+      limitNormal: 3,
+      usageEnhanced: 0.3,
+      limitEnhanced: 3
     },
   ]
 }
